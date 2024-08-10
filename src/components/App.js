2fed746bf61bff725f5fb81c568d0b1da1ccb0d0
suryarajrs <suryars@outlook.in>
2024-08-10 14:26:49 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,31 @@
-import React, {Component, useState} from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+    };
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        { /* Do not remove this main div!! */ }
+        <button id="click" onClick={this.handleClick}>Click</button>
+        {this.state.isClicked && (
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+        )}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
